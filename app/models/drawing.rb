de74a27814b8ecb7801dec6a2d57c08a59ea1e50
moeamaya <moeamaya@gmail.com>
2014-02-13 01:31:44 -0500

    extend drawing about length

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,7 +1,7 @@
 class Drawing < ActiveRecord::Base
   belongs_to :user
   validates :title, length: { minimum: 3 }
-  validates :about, length: { in: 5..500 }
+  validates :about, length: { in: 5..1000 }
 
   mount_uploader :drawing, DrawingUploader
 
